c983043e5acf263bfeb2ddae1e63a553bd75afd0
Grzegor46 <grzegor46@gmail.com>
2023-06-12 22:15:38 +0200

    snake movement #2

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,10 +3,13 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import static java.lang.Thread.sleep;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +17,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	private int squareOfScreen = 50;
 	private OrthographicCamera orthographicCamera;
 	private ShapeRenderer shapeRenderer;
+	private ShapeRenderer shapeRenderer2;
 	private int x=0;
 	private int y=0;
 	private Snake snake;
@@ -22,7 +26,9 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void create () {
+		Gdx.graphics.setContinuousRendering(true);
 		this.shapeRenderer = new ShapeRenderer();
+		this.shapeRenderer2 = new ShapeRenderer();
 		this.widthScreen = Gdx.graphics.getWidth();
 		this.heightScreen = Gdx.graphics.getHeight();
 		this.orthographicCamera = new OrthographicCamera();
@@ -36,17 +42,35 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer2.begin(ShapeRenderer.ShapeType.Filled);
 		shapeRenderer.setColor(80 / 255.0f, 80 / 255.0f, 50 / 255.0f, 1);
+		shapeRenderer2.setColor(70 / 255.0f, 60 / 255.0f, 40 / 255.0f, 1);
+		shapeRenderer2.rect(100,100,50,50);
 		shapeRenderer.rect(snake.getX_pos(), snake.getY_pos(), squareOfScreen, squareOfScreen);
 		snakeMove();
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		shapeRenderer.setProjectionMatrix(orthographicCamera.combined);
 
-		batch.begin();
-		batch.end();
+//		Gdx.gl.glClearColor(0, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shapeRenderer.rect(x, y, squareOfScreen, squareOfScreen);
+		shapeRenderer2.end();
+		shapeRenderer.end();
+
 	}
 
+	private void clearScreen() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
 
 //	TODO: try refactor it
 	private void snakeMove(){
+
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) ) {
 			snake.setX_pos((snake.getX_pos() + -squareOfScreen));
 			this.x = snake.getX_pos();
@@ -68,8 +92,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			shapeRenderer.rect(x, y, squareOfScreen, squareOfScreen);
 		}
 
-		shapeRenderer.rect(x, y, squareOfScreen, squareOfScreen);
-		shapeRenderer.end();
+
 	}
 
 	@Override
