6ac14eb9b2ddf2819f08130cd57f8175e7b79b9c
Grzegor46 <grzegor46@gmail.com>
2023-06-12 18:32:51 +0200

    initial commit Snake game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,40 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture snakeImg;
+	private int widthScreen, heightScreen;
+	private OrthographicCamera orthographicCamera;
+
 	
 	@Override
 	public void create () {
+		this.widthScreen = Gdx.graphics.getWidth();
+		this.heightScreen = Gdx.graphics.getHeight();
+		this.orthographicCamera = new OrthographicCamera();
+		this.orthographicCamera.setToOrtho(false, widthScreen, heightScreen);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		snakeImg = new Texture("snake.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(snakeImg, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		snakeImg.dispose();
 	}
 }
